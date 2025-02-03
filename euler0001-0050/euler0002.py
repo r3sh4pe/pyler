9d40a4438fd08036a36834cc632d9c5ab13ea6fc
r3sh4p3 <john.doe@club.kek>
2025-02-03 13:37:03 +0100

    Problem0002

--- euler0001-0050/euler0002.py
+++ euler0001-0050/euler0002.py
@@ -1,11 +1,19 @@
 import timeit
 import pathlib
 
-def problem0001():
-	pass
+def problem0002():
+	x: int = 1
+	y: int = 2
+	even_sum = 0
+	while y < 4_000_000:
+		if y % 2 == 0:
+			even_sum += y
+		x, y = y, x + y
+	return even_sum
+
 
 if __name__ == '__main__':
-	print(problem0001())
-	time = timeit.timeit(problem0001, number=1_000_000)
+	print(problem0002())
+	time = timeit.timeit(problem0002, number=1_000_000)
 	with open(str(pathlib.Path(__file__).parent.parent) + '/results.csv', 'a') as file:
 		file.write('\n' + str(pathlib.Path(__file__).name) + ', ' + str(time / 1_000_000) + '\n')
